16979c0c44b122e8e3a2f04051b36d0ad37b815a
Ganesh Ghumade <ganeshghumade20062006@gmail.com>
2026-01-02 15:54:55 +0530

    First program in python
    1.Multiple line use
    2.Using \n for new line
    3.Using single quote inside double quote

---
+++ python_for_data_analytics.py
@@ -0,0 +1,45 @@
+# .....
+print(" print statements in python")
+
+
+print("hello woorld")
+print("this is my first program")
+print("i hope you all like it "
+"my name is ganesh"
+"ghumade")
+# hello woorld
+# this is my first program
+# i hope you all like it my name is ganeshghumade
+
+# multiple line use
+print("""hello world
+this is my first program
+ i hope you all like it""")
+# hello world
+# this is my first program
+#  i hope you all like it
+
+# using \n for new line
+print("hello world \nthis is my first program \ni hope you all like it")
+# hello world 
+# this is my first program 
+# i hope you all like it
+
+
+# using single quote inside double quote
+print('it\'s going to rain today')
+# it's going to rain today
+
+# .....
+
+
+
+
+
+
+
+
+
+
+
+
